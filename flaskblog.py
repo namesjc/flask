62d7eb8727285b39ddc2f96569da3ffe15e470b1
CJE5SZH <adiachan@outlook.com>
2018-08-21 10:34:21 +0800

    commit db config

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from models import User, Post
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '86364228fcbf4f9217087814e08790ef'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 posts = [
     {
